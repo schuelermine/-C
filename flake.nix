752b2ca895b2c12d3c405934e3b6e2331747be7c
Anselm Schüler <mail@anselmschueler.com>
2023-08-12 22:47:53 +0200

    Add Nixpkgs overlay

--- flake.nix
+++ flake.nix
@@ -4,13 +4,12 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
   description = "a small utility to run commands in different directories";
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = import nixpkgs { inherit system; };
-    in {
-      packages = rec {
-        "-C" = default;
-        default = pkgs.callPackage ./. { };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in { packages = rec { default = pkgs.callPackage ./. { }; }; }) // {
+        overlays.default = final: prev: {
+          "-C" = self.packages.${self}.default;
+        };
       };
-    }
-  );
 }
